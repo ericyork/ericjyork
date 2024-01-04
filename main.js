4d5da6db15b81922f7cf63fb08c51bb4ff08e1cf
ericyork <eyork@clarkson.edu>
2024-01-04 16:01:10 -0500

    adds support for all pages

--- main.js
+++ main.js
@@ -1,64 +1,38 @@
-// (function(document, history, location) {
-//   var HISTORY_SUPPORT = !!(history && history.pushState);
-//   var anchorScrolls = {
-//     ANCHOR_REGEX: /^#[^ ]+$/,
-//     OFFSET_HEIGHT_PX: 100,
+function show() {
+  let btnText = document.getElementById('btn-text');
+  let container = document.getElementById('m-container');
+  let content = document.getElementById('m-content');
+  container.classList.toggle('clicked');
+  content.classList.toggle('open');
+  if (content.classList.contains('open')) {
+    btnText.innerText = "CLOSE";
+  } else {
+    btnText.innerText = "SEE ALL";
+  }
+}
+function extend(id) {
+  let container = id;
+  container.classList.toggle('extended');
+  let button = container.nextElementSibling;
+  button.classList.toggle('toggled');
+  let buttonText = button.firstElementChild;
+  if (container.classList.contains('extended')) {
+    button.classList.add('hovered');
+    buttonText.innerText = "LESS";
+  } else {
+    button.classList.remove('hovered');
+    buttonText.innerText = "MORE";
+  }
+  console.log(buttonText);
+}
 
-//     init: function() {
-//       this.scrollToCurrent();
-//       window.addEventListener('hashchange', this.scrollToCurrent.bind(this));
-//       document.body.addEventListener('click', this.delegateAnchors.bind(this));
-//     },
-
-//     getFixedOffset: function() {
-//       return this.OFFSET_HEIGHT_PX;
-//     },
-
-//     scrollIfAnchor: function(href, pushToHistory) {
-//       var match, rect, anchorOffset;
-
-//       if(!this.ANCHOR_REGEX.test(href)) {
-//         return false;
-//       }
-
-//       match = document.getElementById(href.slice(1));
-
-//       if(match) {
-//         rect = match.getBoundingClientRect();
-//         anchorOffset = window.pageYOffset + rect.top - this.getFixedOffset();
-//         window.scrollTo(window.pageXOffset, anchorOffset);
-
-//         // Add the state to history as-per normal anchor links
-//         if(HISTORY_SUPPORT && pushToHistory) {
-//           history.pushState({}, document.title, location.pathname + href);
-//         }
-//       }
-
-//       return !!match;
-//     },
-
-//     /**
-//      * Attempt to scroll to the current location's hash.
-//      */
-//     scrollToCurrent: function() {
-//       this.scrollIfAnchor(window.location.hash);
-//     },
-//     /**
-//      * If the click event's target was an anchor, fix the scroll position.
-//      */
-//     delegateAnchors: function(e) {
-//       var elem = e.target;
-
-//       if(
-//         elem.nodeName === 'A' &&
-//         this.scrollIfAnchor(elem.getAttribute('href'), true)
-//       ) {
-//         e.preventDefault();
-//       }
-//     }
-//   };
-
-//   window.addEventListener(
-//     'DOMContentLoaded', anchorScrolls.init.bind(anchorScrolls)
-//   );
-// })(window.document, window.history, window.location);
+// let expanders = document.getElementsByClassName("expander"); // selects all expandable elements
+// let i; // loop counter
+// for (i = 0; i < expanders.length; i++) { // loops through the array
+//   expanders[i].addEventListener("click", function() { // attaches the click handler to an anonymous function
+//     let tog = this.childNodes[1].classList.toggle("opened"); // adds .opened to change :after element
+//     let content = this.childNodes[1].nextElementSibling; // selects content elements
+//     content.classList.toggle("show"); // toggles .show on contents
+//     this.classList.toggle("expanded"); // toggles .expanded on entire element
+//   });
+// }
